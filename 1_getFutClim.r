203ebdc2af759b2ac3a10698fb79dad576b33cc6
Steve Vissault <s.vissault@yahoo.fr>
2017-04-12 15:51:35 -0400

    set script 1 to retrieve clim from quicc_db

--- 1_getFutClim.r
+++ 1_getFutClim.r
@@ -5,22 +5,20 @@ source('./con_quicc_db.r')
 library("RPostgreSQL")
 
 # read list of GCMs
-GCM_df <- read.csv("./data/list_GCMs.csv")
+GCM_df <- read.csv("./data/list_GCM.csv")
 GCM_df <- subset(GCM_df, scenario == 'rcp85')
 
 windows <- seq(2000,2095,5)
-out_folder <- "./data/futClimSTM/"
+out_folder <- "./out_files/futClimSTM/"
 
 for (x in 1:dim(GCM_df)[1]){
     system(paste("mkdir -p ",out_folder,"GCM_id_",rownames(GCM_df)[x],sep=""))
-
     for (i in 1:length(windows)){
-
     query_fut_climData <- paste("SELECT ST_X(geom) as lon, ST_Y(geom) as lat, x , y, var, ", windows[i]-15 ," as min_yr,",windows[i]," as max_yr, val, clim_center, mod, run, scenario FROM (
-    SELECT var,clim_center, mod, run, scenario, (ST_PixelAsCentroids(ST_Union(ST_Clip(ST_Transform(raster,32198),1,env_plots,true),'MEAN'),1,false)).*
+    SELECT var,clim_center, mod, run, scenario, (ST_PixelAsCentroids(ST_Union(ST_Clip(ST_Transform(raster,4269),1,env_plots,true),'MEAN'),1,false)).*
     FROM clim_rs.fut_clim_biovars,
-    (SELECT ST_GeomFromText('POLYGON((-475408.2 194979.3,-475408.2 717179.3,-389008.2 717179.3,-389008.2 194979.3,-475408.2 194979.3))',32198) as env_plots) as envelope
-    WHERE (var='bio1' OR var='bio12') AND (yr>=",windows[i]-15," AND yr<=",windows[i],") AND clim_center='",GCM_df[x,1],"' AND mod='",GCM_df[x,2],"' AND run='",GCM_df[x,3],"' AND scenario='",GCM_df[x,4],"' AND ST_Intersects(ST_Transform(raster,32198),env_plots)
+    (SELECT ST_Transform(ST_GeomFromText('POLYGON((-74.6 45.6,-74.6 50.3,-74 50.3,-74 45.6,-74.6 45.6))',4326),4269) as env_plots) as envelope
+    WHERE (var='bio1' OR var='bio12') AND (yr>=",windows[i]-15," AND yr<=",windows[i],") AND clim_center='",GCM_df[x,1],"' AND mod='",GCM_df[x,2],"' AND run='",GCM_df[x,3],"' AND scenario='",GCM_df[x,4],"' AND ST_Intersects(ST_Transform(raster,4269),env_plots)
     GROUP BY var,clim_center, mod, run, scenario
     ) as pixels;",sep="")
 
